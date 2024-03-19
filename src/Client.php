5d541fc1cc3401070b212b8d3644f338bc282edd
Lars Strojny <lars.strojny@nfq.com>
2024-03-19 14:19:20 +0100

    feat: complete interface

--- src/Client.php
+++ src/Client.php
@@ -5,9 +5,31 @@ declare(strict_types=1);
 namespace Twint\Sdk;
 
 use Twint\Sdk\Value\CertificateValidity;
+use Twint\Sdk\Value\DetectedDevice;
 use Twint\Sdk\Value\MerchantId;
+use Twint\Sdk\Value\Money;
+use Twint\Sdk\Value\Order;
+use Twint\Sdk\Value\OrderId;
+use Twint\Sdk\Value\OrderKind;
+use Twint\Sdk\Value\TransactionReference;
 
 interface Client
 {
     public function getCertificateValidity(MerchantId $merchantId): CertificateValidity;
+
+    public function startOrder(
+        MerchantId $merchantId,
+        Money $requestedAmount,
+        OrderKind $orderKind,
+        TransactionReference $transactionReference,
+    ): Order;
+
+    public function monitorOrderByOrderId(MerchantId $merchantId, OrderId $orderId): Order;
+
+    public function monitorOrderByTransactionReference(
+        MerchantId $merchantId,
+        TransactionReference $transactionReference
+    ): Order;
+
+    public function detectDevice(string $userAgent): DetectedDevice;
 }
