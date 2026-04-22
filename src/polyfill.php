6a1af5d39ab84ea17ebd993c59dbc79e92f5f6f2
Lars Strojny <lars.strojny@gradion.com>
2026-04-22 16:49:49 +0200

    fix(polyfill): prevent collisions with other polyfills that might be loaded earlier

--- src/polyfill.php
+++ src/polyfill.php
@@ -5,21 +5,21 @@ declare(strict_types=1);
 use Twint\Sdk\Certificate\PemCertificate;
 use Twint\Sdk\Certificate\Pkcs8Certificate;
 
-if (PHP_VERSION_ID < 80200) {
+if (PHP_VERSION_ID < 80200 && !class_exists(SensitiveParameter::class)) {
     #[Attribute(Attribute::TARGET_PARAMETER)]
     final class SensitiveParameter
     {
     }
 }
 
-if (PHP_VERSION_ID < 80300) {
+if (PHP_VERSION_ID < 80300 && !class_exists(Override::class)) {
     #[Attribute(Attribute::TARGET_METHOD)]
     final class Override
     {
     }
 }
 
-if (PHP_VERSION_ID < 80400) {
+if (PHP_VERSION_ID < 80400 && !class_exists(Deprecated::class)) {
     #[Attribute(Attribute::TARGET_METHOD | Attribute::TARGET_FUNCTION | Attribute::TARGET_CLASS_CONSTANT)]
     final class Deprecated
     {
